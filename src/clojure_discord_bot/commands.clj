a352576bfba3654a9e50a2128e6a68413e1899a5
matievisthekat <matievisthekat@gmail.com>
2022-12-03 13:41:31 +0200

    remove weird code

--- src/clojure_discord_bot/commands.clj
+++ src/clojure_discord_bot/commands.clj
@@ -1,10 +1,5 @@
 (ns clojure-discord-bot.commands
-  (:require [discljord.messaging :as msg]))
+  (:require [slash.command :as cmd]))
 
-(defn create-commands [conn app-id]
-  (msg/create-global-application-command!
-   conn
-   app-id
-   "help"
-   "View the commands available for this bot"
-   {:default-permission :send-messages}))
+
+
